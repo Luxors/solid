28a39d5551877b3f4a1506b489ef02ab5bcb4dfa
Yaroslav V. Derkach <Luxors@mail.com>
2026-04-08 22:11:08 +0300

    fun-box: add Card

--- sol-fun-box/src/App.tsx
+++ sol-fun-box/src/App.tsx
@@ -1,12 +1,41 @@
-import type { Component } from 'solid-js';
+import { type Component, For } from 'solid-js';
+
+import { ICard } from './utils/types';
 import Card from './components/Card';
+
+const products: ICard[] = [
+  {
+    id: 1,
+    consist: 'фуа-гра',
+    quantity: 10,
+    weight: '0,5',
+    slogan: 'Печень утки разварная с артишоками.',
+  },
+  {
+    id: 2,
+    consist: 'рыбой',
+    quantity: 40,
+    gift: 2,
+    weight: '2',
+    slogan: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+  },
+  {
+    id: 3,
+    consist: 'курой',
+    quantity: 100,
+    gift: 5,
+    weight: '5',
+    isDisabled: true,
+    slogan: null,
+  },
+];
 
 const App: Component = () => {
   return (
     <main class="app-main">
       <h1 class="app-title">Ты сегодня покормил кота?</h1>
       <div class="app-products">
-        <Card />
+        <For each={products}>{(item) => <Card {...item} />}</For>
       </div>
     </main>
   );
--- sol-fun-box/src/components/Card.tsx
+++ sol-fun-box/src/components/Card.tsx
@@ -1,3 +1,42 @@
-export default () => {
-  return <h2>Card component</h2>;
-};
+import { plural } from '../utils/plural';
+import { ICard } from '../utils/types';
+
+import catImage from '../assets/images/cat.png';
+
+export default function Card(props: ICard) {
+  return (
+    <article>
+      <div class="card-wrapper">
+        <div class="card-background"></div>
+        <div class="card-product">
+          <div class="card-promo">сказочное заморское яство</div>
+          <h2 class="card-title">нямушка</h2>
+          <div class="card-consist">с {props.consist}</div>
+          <ul class="card-includ">
+            <li class="card-includ__item">
+              <b class="card-includ__item--bold">{props.quantity} </b>
+              порций
+            </li>
+            <li class="card-includ__item">
+              {props.gift && <b class="card-includ__item--bold">{props.gift}</b>}{' '}
+              {plural(props.gift, ['мышь', 'мыши', 'мышей'])} в подарок
+            </li>
+          </ul>
+        </div>
+        <img class="card-image" src={catImage} alt="Нямушка" />
+        <div class="card-weight">
+          <span class="card-weight__num">{props.weight}</span>
+          <span class="card-weight__label">кг</span>
+        </div>
+      </div>
+      {/* <footer class="card-footer">
+    <CardSlogan
+      slogan={card.slogan}
+      isSelected={isSelected}
+      isDisabled={card.isDisabled}
+      onClick={toggleSelected}
+    />
+  </footer> */}
+    </article>
+  );
+}
