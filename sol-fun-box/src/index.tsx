97af085688405c076b7cb0a84acdeb0d3ada28ab
Yaroslav V. Derkach <Luxors@mail.com>
2026-04-08 20:22:22 +0300

    fun-box: add assets

--- sol-fun-box/src/index.tsx
+++ sol-fun-box/src/index.tsx
@@ -2,13 +2,15 @@
 import { render } from 'solid-js/web';
 import 'solid-devtools';
 
+import './assets/styles/base.css';
+
 import App from './App';
 
 const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?'
   );
 }
 
